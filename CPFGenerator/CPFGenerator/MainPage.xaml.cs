deda4f4e984e9c78f886a9e77ab985b806aea794
henquesz <henquesvinicius@gmail.com>
2022-04-15 21:13:57 -0300

    first production commit

---
+++ CPFGenerator/CPFGenerator/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CPFGenerator
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public bool IsPlaying { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            IsPlaying = !IsPlaying;
+            OnPropertyChanged(nameof(IsPlaying));
+        }
+        private void btn_gerar_Clicked(object sender, EventArgs e)
+        {
+            var random = new Random();
+
+            int soma = 0;
+            int resto = 0;
+            int[] multiplicadores = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string semente;
+
+            do
+            {
+                semente = random.Next(1, 999999999).ToString().PadLeft(9, '0');
+            }
+            while (
+                semente == "000000000"
+                || semente == "111111111"
+                || semente == "222222222"
+                || semente == "333333333"
+                || semente == "444444444"
+                || semente == "555555555"
+                || semente == "666666666"
+                || semente == "777777777"
+                || semente == "888888888"
+                || semente == "999999999"
+            );
+
+            for (int i = 1; i < multiplicadores.Count(); i++)
+                soma += int.Parse(semente[i - 1].ToString()) * multiplicadores[i];
+
+            resto = soma % 11;
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            semente += resto;
+            soma = 0;
+
+            for (int i = 0; i < multiplicadores.Count(); i++)
+                soma += int.Parse(semente[i].ToString()) * multiplicadores[i];
+
+            resto = soma % 11;
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            semente = semente + resto;
+
+            semente = Convert.ToUInt64(semente).ToString(@"000\.000\.000\-00");
+
+            lbl_resultado.Text = semente;
+
+        }
+
+    }
+
+ }
